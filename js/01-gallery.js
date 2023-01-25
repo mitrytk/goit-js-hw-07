c84f73ed923b43cc8671bc458b1cd7bc446288d8
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2023-01-25 15:54:08 +0100

    add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const markup = [];
+const galleryListEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+galleryItems.map( item => isMakeItem(item));
+galleryListEl.append(...markup);
+galleryListEl.addEventListener("click", onImgClick);
+console.log(galleryListEl);
+
+function isMakeItem({preview, original, description}) {
+    const divGalleryItemEl = document.createElement('div');
+    divGalleryItemEl.classList.add('gallery__item');
+
+    const aGalleryLinkEl = document.createElement('a');
+    aGalleryLinkEl.classList.add('gallery__link');
+    // aGalleryLinkEl.href = original;
+    divGalleryItemEl.append(aGalleryLinkEl);
+
+    const imgGalleryEl = document.createElement('img');
+    imgGalleryEl.classList.add('gallery__image');
+    imgGalleryEl.src = preview;
+    imgGalleryEl.dataset.source = original;
+    imgGalleryEl.alt = description;
+    aGalleryLinkEl.append(imgGalleryEl);
+
+    markup.push(divGalleryItemEl);
+}
+
+function onImgClick(evt) {
+    const isImgEl = evt.target.classList.contains("gallery__image");
+    console.log(isImgEl);
+    if (!isImgEl) {
+        return;
+    };
+    const imgSrc = evt.target.dataset.source;
+    console.log(imgSrc);
+    const instance = basicLightbox.create(`
+    <img src= ${imgSrc} >
+    `);
+    instance.show();
+}
+
+/* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div> */
