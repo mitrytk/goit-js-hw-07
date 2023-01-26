248707e3a0e48bac8edca4d91af6a7f0f491b352
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2023-01-26 09:19:19 +0100

    performs task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const markup = [];
+const galleryListEl = document.querySelector('.gallery');
 
+galleryItems.map( item => isMakeItem(item));
+galleryListEl.append(...markup);
+const lightbox = new SimpleLightbox('.gallery li a', 
+    {fadeSpeed: 250,
+    captionsData: "alt",});
+
+function isMakeItem({preview, original, description}) {
+    const liGalleryItemEl = document.createElement('li');
+    liGalleryItemEl.classList.add('gallery__item');
+
+    const aGalleryLinkEl = document.createElement('a');
+    aGalleryLinkEl.classList.add('gallery__link');
+    aGalleryLinkEl.href = original;
+    liGalleryItemEl.append(aGalleryLinkEl);
+
+    const imgGalleryEl = document.createElement('img');
+    imgGalleryEl.classList.add('gallery__image');
+    imgGalleryEl.src = preview;
+    imgGalleryEl.alt = description;
+    aGalleryLinkEl.append(imgGalleryEl);
+
+    markup.push(liGalleryItemEl);
+}
 console.log(galleryItems);
